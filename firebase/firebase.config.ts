b8140fe415555fce1573312488dd290573dbe7a0
Rafael Redigolo <rafael_redigolo@hotmail.com>
2016-11-26 06:56:33 -0200

    Update firebase.config.ts

--- firebase/firebase.config.ts
+++ firebase/firebase.config.ts
@@ -19,5 +19,35 @@ export class Fire {
   getDb() {    
     return Firebase;
   }
+  
+  login(token: string, successCallback, errorCallback){
+    let credential = Firebase.auth.FacebookAuthProvider.credential(token);
+
+    Firebase.auth().signInWithCredential(credential).then((response) => {
+      this.setUser(token, response.providerData[0]);
+      successCallback(response);
+    }, (error) => {
+      errorCallback(error);
+    })
+  }
+
+  loginStatus(successCallback, errorCallback) {
+    Firebase.auth().onAuthStateChanged((user) => {
+      if(user){
+        this.setUser("", user);
+        successCallback(user);
+      } else {
+        errorCallback('error');
+      }
+    })
+  }
+
+  private setUser(token: string, authData: any) {
+    this.user.name = authData.displayName;
+    this.user.email = authData.email;
+    this.user.photo = authData.photoURL;
+    this.user.id = authData.uid;
+    this.user.token = token;
+  }
 
 }
